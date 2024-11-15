4fef46e0cba2cb7d7dad3a50eb7a2cca1b2fd01b
twinsv4 <shinja0829@naver.com>
2024-11-15 14:52:08 +0900

    1

---
+++ src/main/frontreact/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot + React + NCP</h1>
+      <h3>카운트 : 2</h3>
+      <img src="./image/band.jpg" alt="빵빵이" />
+    </div>
+  );
+};
+
+export default App;
